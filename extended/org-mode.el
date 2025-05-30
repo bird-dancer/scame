f01e31cb96a7e768a88f1006e8f1a5302cd78900
Felix Dumbeck <felix@dumbeck.net>
2025-05-30 18:06:30 +0200

    extended: fix org-mode

--- extended/org-mode.el
+++ extended/org-mode.el
@@ -1,2 +1,8 @@
 (use-package org-cliplink
   :bind ("C-x p i" . org-cliplink))
+
+(use-package org-download
+  :hook ((dired-mode . org-download-enable)))
+
+(use-package org-auto-tangle
+  :hook (org-mode . org-auto-tangle-mode))
