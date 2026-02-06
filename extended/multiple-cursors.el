ccd3211c6941d1ee8b938bd0c95d9954cee137c5
Felix Dumbeck <felix@dumbeck.net>
2026-02-06 01:05:25 +0100

    extended: mc dont ask if to run for all

--- extended/multiple-cursors.el
+++ extended/multiple-cursors.el
@@ -1,8 +1,10 @@
+(setq mc/always-run-for-all t)
 (use-package multiple-cursors
   :bind (("C-;" . mc/edit-lines)	;non standard
          ("C-S-c C-S-c" . mc/edit-lines)
          ("C->" . mc/mark-next-like-this)
          ("C-<" . mc/mark-previous-like-this)
          ("C-c C-<" . mc/mark-all-like-this))
-  :init
-  (setq  mc/match-cursor-style nil))	;or else cursors dont show up when style is bar
+  :config
+  ;; or else cursors dont show up when style is bar
+  (setq mc/match-cursor-style nil))
